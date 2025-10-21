f8c8cdd11b0fa127a88c29429f14e8079bf4ef06
tainara.luza <tessdev3@gmail.com>
2025-10-21 12:21:49 +0100

    Initial commit

---
+++ src/main/java/io/github/tessla2/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,33 @@
+package io.github.tessla2.libraryapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    }
+
+}
