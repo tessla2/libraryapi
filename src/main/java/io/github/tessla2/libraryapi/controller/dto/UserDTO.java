31065687025a43441bb205a2012fcb5ec6ebd25b
tainara.luza <tessdev3@gmail.com>
2026-04-24 16:35:47 +0100

    roles and basic auth

---
+++ src/main/java/io/github/tessla2/libraryapi/controller/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package io.github.tessla2.libraryapi.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public record UserDTO(
+        UUID id,
+        @NotBlank(message = "Required field")
+        @Email(message = "Invalid email format")
+        @Size(max = 100, message = "Field exceeds the maximum allowed length")
+        String email,
+        @NotBlank(message = "Required field")
+        @Size(min = 6, max = 100, message = "Field exceeds the maximum allowed length")
+        String password,
+        java.util.List<String> roles) {
+}
